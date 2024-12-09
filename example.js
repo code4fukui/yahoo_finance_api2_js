919e8b2a65eca67a61dcda61f41170383f8073f5
Taisuke Fukuno <fukuno@jig.jp>
2024-12-10 08:45:16 +0900

    to js

--- example.js
+++ example.js
@@ -1,19 +1,15 @@
-import pprint
-import sys
-from yahoo_finance_api2 import share
-from yahoo_finance_api2.exceptions import YahooFinanceError
+import * as share from "./yahoo_finance_api2/share.js";
 
-my_share = share.Share('NBR')
-symbol_data = None
+const my_share = new share.Share('NBR');
+let symbol_data = null;
 
-try:
+try {
     symbol_data = my_share.get_historical(share.PERIOD_TYPE_DAY,
                                           10,
                                           share.FREQUENCY_TYPE_MINUTE,
-                                          5)
-except YahooFinanceError as e:
-    print(e.message)
-    sys.exit(1)
-
-pp = pprint.PrettyPrinter(depth=6)
-pp.pprint(symbol_data)
+                                          5);
+} catch (e) {
+    console.log(e.message);
+    Deno.exit(1);
+}
+console.log(JSON.stringify(symbol_data, null, 2));

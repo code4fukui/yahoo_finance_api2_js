312fc667704483189b3f7d3fd2a96d0dbf175148
Taisuke Fukuno <fukuno@jig.jp>
2024-12-10 09:24:58 +0900

    refine

--- example.js
+++ example.js
@@ -1,15 +1,10 @@
 import * as share from "./share.js";
 
-const my_share = new share.Share('NBR');
-let symbol_data = null;
-
-try {
-    symbol_data = await my_share.get_historical(share.PERIOD_TYPE_DAY,
-                                          10,
-                                          share.FREQUENCY_TYPE_MINUTE,
-                                          5);
-} catch (e) {
-    console.log(e.message);
-    Deno.exit(1);
-}
+const my_share = new share.Share("NBR");
+const symbol_data = await my_share.get_historical(
+    share.PERIOD_TYPE_DAY,
+    10,
+    share.FREQUENCY_TYPE_MINUTE,
+    5
+);
 console.log(JSON.stringify(symbol_data, null, 2));
